1f4472d8dc8281f1ff241fe75bc0cf806861981a
GimenaRivoira <gimenarivoira@gmail.com>
2020-12-18 09:40:51 -0300

    ejercicio es par

---
+++ esPar.js
@@ -0,0 +1,10 @@
+let numeroPar = function (numero){
+    if (numero == 0){
+    console.log ('syntax error')
+}else {
+numero % 2 == 0 ? console.log ('El numero es par') : console.log ('El numero es impar');
+}
+}
+
+
+numeroPar()
